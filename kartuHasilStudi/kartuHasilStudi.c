52345488c3a513a42cae71f03fc049ce28f91960
Xifted <oneisfirst01@gmail.com>
2023-11-08 14:40:36 +0700

    CodeBlocks Fix

--- kartuHasilStudi/kartuHasilStudi.c
+++ kartuHasilStudi/kartuHasilStudi.c
@@ -1,3 +1,8 @@
+// 1. Dariel Muhammad(2023071048)
+// 2. Muhammad Rafi Irfan(2023071053)
+// 3. Ferdinan Indra Ferta(2023071059)
+// 4. Raihan Ilham Ramadhan(2023071075)
+
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
@@ -7,12 +12,12 @@
 
 COORD coord = { 0, 0 };
 int nmr;
-char kodeMK[7][10];
-char namaMK[7][30];
-int sks[7];
-char grade[7][3];
-float mutu[7];
-float SxM[7];
+char kodeMK[10];
+char namaMK[30];
+int sks;
+char grade[3];
+float mutu;
+float SxM;
 int totalSKS = 0;
 float totalSxM = 0;
 float IPS = 0;
@@ -23,56 +28,88 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+void inputKodeMK() {
+    scanf_s("%s", &kodeMK, 10);
+}
+
+void inputNamaMK() {
+    scanf_s("%s", &namaMK, 30);
+}
+
+void inputSKS() {
+    scanf_s("%d", &sks);
+}
+
+void inputGrade() {
+    scanf_s("%s", &grade, 3);
+}
+
+void inputMatkul() {
+    for (nmr = 1; nmr <= 7; nmr++) {
+        gotoxy(0, 3 + nmr); printf("%d.", nmr);
+
+        // Input data
+        gotoxy(6, 3 + nmr); inputKodeMK();
+        gotoxy(16, 3 + nmr); inputNamaMK();
+        gotoxy(27, 3 + nmr); inputSKS();
+        gotoxy(33, 3 + nmr); inputGrade();
+
+        // Perhitungan Mutu
+        if (strcasecmp(grade, "A") == 0) {
+            mutu = 4.0;
+        }
+        else if (strcasecmp(grade, "A-") == 0) {
+            mutu = 3.7;
+        }
+        else if (strcasecmp(grade, "B+") == 0) {
+            mutu = 3.3;
+        }
+        else if (strcasecmp(grade, "B") == 0) {
+            mutu = 3.0;
+        }
+        else if (strcasecmp(grade, "B-") == 0) {
+            mutu = 2.7;
+        }
+        else if (strcasecmp(grade, "C+") == 0) {
+            mutu = 2.3;
+        }
+        else if (strcasecmp(grade, "C") == 0) {
+            mutu = 2.0;
+        }
+        else if (strcasecmp(grade, "C-") == 0) {
+            mutu = 1.7;
+        }
+        else if (strcasecmp(grade, "D") == 0) {
+            mutu = 1.0;
+        }
+        else if (strcasecmp(grade, "E") == 0) {
+            mutu = 0;
+        }
+        else {
+            printf("INVALID");
+            mutu = 0;
+        }
+
+        // Perhitungan SxM
+        SxM = mutu * sks;
+
+        // Hasil Mutu dan SxM 
+        gotoxy(40, 3 + nmr); printf("%.2f", mutu);
+        gotoxy(48, 3 + nmr); printf("%.2f", SxM);
+
+        // Total SKS, total SxM
+        totalSKS += sks;
+        totalSxM += SxM;
+    }
+}
+
 int main() {
     printf("Kartu Hasil Studi");
     printf("\n------------------------------------------------------");
     printf("\nNo.   Kode MK   Nama MK   SKS   Grade   Mutu    SxM");
     printf("\n------------------------------------------------------");
 
-    for (nmr = 1; nmr <= 7; nmr++) {
-        gotoxy(0, 3 + nmr); printf("%d.", nmr);
-
-        // Input data
-        gotoxy(6, 3 + nmr); scanf_s("%s", &kodeMK[nmr], 10);
-        gotoxy(16, 3 + nmr); scanf_s("%s", &namaMK[nmr], 30);
-        gotoxy(27, 3 + nmr); scanf_s("%d", &sks[nmr]);
-        gotoxy(33, 3 + nmr); scanf_s("%s", &grade[nmr], 3);
-
-        // Perhitungan Mutu
-        if (strcasecmp(grade[nmr], "A") == 0) {
-            mutu[nmr] = 4.0;
-        }else if (strcasecmp(grade[nmr], "A-") == 0) {
-            mutu[nmr] = 3.7;
-        }else if (strcasecmp(grade[nmr], "B+") == 0) {
-            mutu[nmr] = 3.3;
-        }else if (strcasecmp(grade[nmr], "B") == 0) {
-            mutu[nmr] = 3.0;
-        }else if (strcasecmp(grade[nmr], "B-") == 0) {
-            mutu[nmr] = 2.7;
-        }else if (strcasecmp(grade[nmr], "C+") == 0) {
-            mutu[nmr] = 2.3;
-        }else if (strcasecmp(grade[nmr], "C") == 0) {
-            mutu[nmr] = 2.0;
-        }else if (strcasecmp(grade[nmr], "C-") == 0) {
-            mutu[nmr] = 1.7;
-        }else if (strcasecmp(grade[nmr], "D") == 0) {
-            mutu[nmr] = 1.0;
-        }else if (strcasecmp(grade[nmr], "E") == 0) {
-            mutu[nmr] = 0;
-        }else {
-            printf("INVALID");
-            mutu[nmr] = 0;
-        }
-
-        // Perhitungan SxM, total SKS, total SxM
-        SxM[nmr] = mutu[nmr] * sks[nmr];
-        totalSKS += sks[nmr];
-        totalSxM += SxM[nmr];
-
-        // Hasil Mutu dan SxM 
-        gotoxy(40, 3 + nmr); printf("%.2f", mutu[nmr]);
-        gotoxy(48, 3 + nmr); printf("%.2f", SxM[nmr]);
-    }
+    inputMatkul();
 
     printf("\n------------------------------------------------------");
     printf("\nTotal");
